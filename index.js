d4fa445eb2f127284413fcf643ec2a7a604e7a06
Danush Ramanan <dan.ramanan@gmail.com>
2023-01-24 09:18:39 -0700

    serve html on the interface

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const port = 3000;
+
+// create interface
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+// get input from interface
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
