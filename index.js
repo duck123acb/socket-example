f1437cf2e1d44789c63793f7ced4d158c74f36ee
Danush Ramanan <dan.ramanan@gmail.com>
2023-01-24 09:20:58 -0700

    serve a file instead of a sting of html

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = 3000;
 
 // create interface
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+	res.sendFile("./index.html");
 });
 
 // get input from interface
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
