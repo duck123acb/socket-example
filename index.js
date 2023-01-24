28e23d4c427fb73bdaf30b100b8e657f8fe1310c
Danush Ramanan <dan.ramanan@gmail.com>
2023-01-24 09:42:10 -0700

    communication between interface/client and server

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const server = http.createServer(app);
 const port = 3000;
 
 // create interface
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", socket => {
+	console.log("user connected");
 });
 
 // get input from interface
