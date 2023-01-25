a10d40e4831aeae6e5ca892aa6a0d1b1447106dd
Danush Ramanan <dan.ramanan@gmail.com>
2023-01-25 08:05:45 -0700

    stores messages

--- index.js
+++ index.js
@@ -7,23 +7,33 @@ const io = new Server(server);
 
 const port = 3000;
 
+let messages = [];
+
 // create interface
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", socket => { // socket is client
+io.on("connection", socket => {
 	console.log("user connected");
 
 	let name = "chat name";
 	let message = "placeholder";
+
+	socket.on("user joined", msg => {
+		messages.forEach((message) => {
+                	io.to(msg).emit("recive message", message);
+        	});
+	});
 
 	socket.on("chat name", msg => {
 		name = msg;
 	});
 
 	socket.on("chat message", msg => {
-		io.emit("recive message", `${name}: ${msg}`);
+		const message = `${name}: ${msg}`;
+		messages.push(message);
+		io.emit("recive message", message);
 	});
 
 	socket.on('disconnect', () => {
