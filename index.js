d0274d5cc99e7f956728e3bb74037a92ee2320ad
Danush Ramanan <dan.ramanan@gmail.com>
2023-01-24 17:15:31 -0700

    can now send messages between client and server

--- index.js
+++ index.js
@@ -12,8 +12,23 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
+io.on("connection", socket => { // socket is client
 	console.log("user connected");
+
+	let name = "chat name";
+	let message = "placeholder";
+
+	socket.on("chat name", msg => {
+		name = msg;
+	});
+
+	socket.on("chat message", msg => {
+		console.log(`${name}: ${msg}`);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
 });
 
 // get input from interface
